6146cd5409e2bc6c2511f319becf97469e6796a9
Mary-Vivian <muthonimaryvivian>
2024-07-20 21:25:30 +0300

    Created a Restarunt website

---
+++ src/app/home/page.tsx
@@ -0,0 +1,214 @@
+
+function HomePage(){
+    return(
+        <>
+        <div className="flex gap-20  justify-center items-center mt-8 ">
+            <div className="text-6xl scroll-my-0">
+            <h1>Enjoy Delicious</h1>
+            <h1>Food in <b className="text-amber-400">Healthy Life</b></h1>
+            <br/>
+            <div className="text-left">
+            <p className="text-3xl">Tandoori masala is an Indian spice blend consisting of a</p>
+            <p className="text-3xl">variety of spices Tandoori masala as an indian space blend</p>
+            <p className="text-3xl">consisting of a variety of spices</p>
+            <button className="bg-orange-600 mt-5 p-3 rounded-lg text-white text-2xl">ORDER NOW</button>
+            </div>
+            </div>
+          
+            <div className="mt-4 ">
+                <img src="./images/plate1.png" width={500}/>
+            </div>
+    
+        </div>
+        <div className="text-center text-5xl mt-8">
+            <h1 >Popular Foods</h1>
+            </div>
+        <div className="flex gap-20 justify-center items-center mt-6 m-24 ">
+            <div>
+             <img src="./images/fruitfish.png" width={200}/>
+             <p className="text-2xl">Fruit dish</p>
+             <p className="text-2xl">Over food</p>
+             <button className="bg-orange-600 mt-5 p-2 rounded-lg text-white text-sm">Add to cart $400</button>
+            </div>
+            <div>
+            <img src="./images/fruitdish.png" width={200}/>
+            <p className="text-2xl">Fruit dish</p>
+             <p className="text-2xl" >Over food</p>
+             <button className="bg-orange-600 mt-5 p-2 rounded-lg text-white text-sm">Add to cart $400</button>
+            </div>
+            <div>
+            <img src="./images/seafish.png" width={200}/>
+            <p className="text-2xl">Fruit dish</p>
+             <p className="text-2xl">Over food</p>
+             <button className="bg-orange-600 mt-5 p-2 rounded-lg text-white text-sm">Add to cart $400</button>
+    
+            </div>
+            <div>
+            <img src="./images/pizza.png" width={200}/>
+            <p className="text-2xl">Fruit dish</p>
+             <p className="text-2xl">Over food</p>
+             <button className="bg-orange-600 mt-5 p-2 rounded-lg text-white text-sm">Add to cart $400</button>
+            </div>
+
+        </div>
+        <div className="flex justify-center items-center">
+            <div className="mt-8 m-20">
+                <h1 className="text-6xl">Our Special Offer</h1>
+                <p className="mt-8 text-4xl">Best cooks and best delivery guys at your</p>
+                <p className="text-4xl">service. Hot tasty food will reach you in 60 minutes.</p>
+                <button className="bg-orange-600 mt-5 p-2 rounded-lg text-white text-sm">See All Menu</button>
+            </div>
+            <div>
+                <img src="./images/menu.png" width={400}/>
+            </div>
+        </div>
+        
+            <div className="mt-8 text-center">
+                <h1 className="text-6xl">Get The Best Offers</h1>
+                <p className="text-2xl">The food at your doorstep. why starve when you have us. Your hunger</p>
+                <p className="text-2xl">patrner, Straight out of the oven to your dorstep</p>
+        </div>
+        <div className="flex gap-20 text-left mt-12 m-24 text-2xl justify-center items-center">
+           <div className="flex">
+            <div >
+            <h3>Any day offers</h3>
+            <p>New phenomenon</p>
+            <p>Burger taste</p>
+            <p>$ 12.90</p>
+            </div>
+            <div>
+            <img src="./images/ham.jpeg" width={200}/>
+            </div>
+           </div>
+           <div className="flex gap-20 text-left  text-2xl">
+            <div>
+            <h3>Other</h3>
+            <p>flavors</p>
+            <p>save room. We</p>
+            <p>Made salad</p>
+            <p>$ 12.00</p>
+            </div>
+            <div>
+            <img src="./images/salad.png" width={200}/>
+            </div>
+           </div>
+           <div className="flex gap-20 text-left text-2xl">
+            <div>
+            <h3>Find a poco</h3>
+            <p>Store near</p>
+            <p>You</p>
+            <p>$ 12.90</p>
+            </div>
+            <div>
+            <img src="./images/poco.png" width={200}/>
+            </div>
+           </div>
+            </div>
+            <div>
+                <h1 className="text-center text-4xl">Our Service</h1>
+            </div>
+          <div className="flex gap-20 mt-8 m-24 justify-center items-center">
+            <div>
+            <img src="./images/spoon.png" width={200}/>
+            <h3 className="text-3xl">55+ Restaurants</h3>
+            </div>
+            <div>
+            <img src="./images/rate.png" width={200}/>
+            <h3 className="text-3xl">Good quality</h3>
+            </div>
+            <div>
+            <img src="./images/cover.png" width={200}/>
+            <h3 className="text-3xl">Discount system</h3>
+            </div>
+            <div>
+            <img src="./images/cart.png" width={200}/>
+            <h3 className="text-3xl">fast delivery</h3>
+            </div>
+            </div>
+
+            <div className="text-center ">
+                <p className="text-2xl">Testimonial</p>
+                <h1 className="text-4xl">What Our Clients Say</h1>
+            </div>
+            <div className="mt-9 m-28 text-center text-2xl justify-center items-center">
+                <p>Lorem ipsum dolor aimet,consetus adipisingelit,sed do</p>
+                <p>ut labore et dalore magna alique. Uyt ad  mininms quies nagfhj</p>
+                <p>ulicamo laboris nisi ut alique ea commendo</p>
+                <div className="flex  resize-y  gap-20 mt-8 ms-9 justify-center items-center">
+                    <div>
+                        <img   src="./images/profile.png" width={50}/>
+                    </div>
+                    <div>
+                        <img src="./images/profile.png" width={50}/>
+                    </div>
+                    <div>
+                        <img src="./images/profile.png" width={50}/>
+                    </div>
+                    <div>
+                        <img src="./images/profile.png" width={50}/>
+                    </div>
+                </div>
+            </div>
+            <div className="flex gap-28 m-24 justify-center items-center">
+                <div>
+                    <img src="./images/phone.jpeg"/>
+                </div>
+                <div>
+                    <h1 className="text-6xl">It's all here.</h1>
+                    <h1 className="text-6xl">All in one app.</h1>
+                   <p className="mt-8">Discover local, on-demand delivery or pickup from</p>
+                   <p>restaurants, nearby grocery and convenience stores</p>
+                   <p>and more</p>
+                </div>
+            </div>
+            <div className="flex gap-28 m-24 justify-center items-center">
+            <div>
+                <h1 className="text-4xl">Subscribe Our Newsletter</h1>
+                <p className="mt-8">Subscribe on our newsletter to get our</p>
+                <p>news</p>
+            </div>
+            <div>
+                <p>Your email address</p>
+                <button className="bg-orange-600 mt-5 p-2 rounded-lg text-white text-sm">Subscribe</button>
+            </div>
+            </div>
+            <div className="flex gap-20 bg-black text-white justify-center items-center ">
+                <div className="ms-18">
+                    <h1>FoodHouse.</h1>
+                    <p>Best cooks and best delivery guys all</p>
+                    <p>at your service. Hot tasty food will</p>
+                    <p>reach you in 60 minutes.</p>
+                </div>
+                <div>
+                    <h3>Company</h3>
+                    <p>Career</p>
+                    <p>About Us</p>
+                    <p>Blog</p>
+                    <p>Press Info</p>
+                    <p>Features</p>
+                </div>
+                <div>
+                    <h3>Fudo</h3>
+                    <p>Why fudo</p>
+                    <p>How it works</p>
+                    <p>Why choose Us</p>
+                    <p>Client Stories</p>
+                    <p>Gallery</p>
+                </div>
+                <div>
+                    <h3>Menu</h3>
+                    <p>Breakfast</p>
+                    <p>Lunch</p>
+                    <p>Dinner</p>
+                    <p>Fast Foods</p>
+                    <p>Drinks</p>
+                </div>
+            </div>
+            <div className="bg-amber-400 text-white ">
+                <p className="text-center">Copyright 2021 Bensik All Right Reserved</p>
+            </div>
+        </>
+
+    )
+}
+export default  HomePage;
---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Navbar = () =>{
+    return(
+        <div className=" gap-10 flex-row flex justify-evenly">
+            <div>
+            <a>HOME</a>
+            <a>RESTRUNT</a>
+            <a>SERVICES</a>
+            <a>CART</a>
+            </div>
+        </div>
+    )
+
+}
+export default Navbar;
